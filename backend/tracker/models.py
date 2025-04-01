d464c36adb731577007cb8b3f05f4731dd558d10
Aashim B <ashimbazracharya@gmail.com>
2025-04-01 10:02:04 +0545

    Backend: Create CalorieEntry model

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CalorieEntry(models.Model):
+    food_name = models.CharField(max_length=255)
+    calories = models.PositiveIntegerField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.food_name} - {self.calories} kcal"

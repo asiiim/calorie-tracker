a3c8ebbb06b516e6422aadcf57a867db1a8ea59b
Aashim B <ashimbazracharya@gmail.com>
2025-04-01 10:04:15 +0545

    Backend: Implement CalorieEntry API view

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CalorieEntry
+from .serializers import CalorieEntrySerializer
 
-# Create your views here.
+class CalorieEntryViewSet(viewsets.ModelViewSet):
+    queryset = CalorieEntry.objects.all()
+    serializer_class = CalorieEntrySerializer
